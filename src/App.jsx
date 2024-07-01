705013c5ba794f8135a834587c42a20bc2eeb27d
MitsuiMinoru <mitsuimino711@gamil.com>
2024-07-02 04:29:26 +1000

    Update...

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 
+import HeadNotify from './components/HeadNotify'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-black text-white'>23423</div>
+      <HeadNotify />
     </>
   )
 }
---
+++ src/components/HeadNotify.jsx
@@ -0,0 +1,13 @@
+
+const HeadNotify = () => {
+    return (
+        <>
+            <div className="bg-black text-white w-screen flex justify-center items-center h-12 relative">
+              <span>Read FAQs on the About page before subscribing.</span>
+              <button className="absolute right-4">X</button>
+            </div>
+        </>
+    )
+}
+
+export default HeadNotify;
